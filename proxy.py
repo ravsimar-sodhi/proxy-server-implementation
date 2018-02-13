c54e933cdc93bcff191a3f386ed45e5878248376
Ravsimar Singh Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2018-02-13 12:41:16 +0530

    refactored code

--- proxy.py
+++ proxy.py
@@ -2,91 +2,90 @@ from socket import *
 import os
 import time
 
-cacheData = {}
-#Create a server socket, bind it to a port and start listening
-soc = socket(AF_INET, SOCK_STREAM) #Initializing socket
-soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-soc.bind(("", 12345)) #Binding socket to port
-soc.listen(5) #Listening for page requests
-while True:
-    #Start receiving data from the client
-    print 'Listening ...'
-    clientSoc, addr = soc.accept()
-    print 'Received connection from: ', addr
-    message = clientSoc.recv(1024)
-    # print "message:",message
+def recvResponse(soc):
+    response = ""
+    while True:
+        resp = soc.recv(1024)
+        if not resp:
+            break
+        response += resp
+    return response
+        
+def parseRequest(request):
+    url = request.split()[1] # This gives the whole url e.g :http://host:port/filename
+    try:
+        filename = url.partition("/")[2] # e.g host:port/filename
+    # if url is of the form http://host:port , the above will give an index out of bound error
+    except:
+        filename = url[6:] # host:port
+    return filename
 
-    #Extract the filename from the given message
-    url = message.split()[1]
-    print url
-    filename = url.partition("/")[2]
-    # print "filename:",filename
-    # fileExist = "false"
-    filetouse = "/" + filename
-    # print "filetouse:",filetouse
+def getIMSHeader(ind,val):
+    timestamp = val[ind+15:ind+15+29] # Get the cache Last-Modified header to check validity of cache
+    tempTime = time.strptime(timestamp, "%a, %d %b %Y %H:%M:%S %Z") # Convert to struct_time object
+    tempTime = time.localtime(time.mktime(tempTime) + 3600*5.5) # Convert from GMT Time to local time by adding 5:30 hours
+    timestamp = time.strftime("%a %b %d %H:%M:%S %Y",tempTime) # Convert to string
+    return "If-Modified-Since: " + timestamp # Final header
+
+def addHeader(request, header):
+    splitMess = request.split('\n')
+    splitMess.insert(3,header)
+    modMessage = '\n'.join(splitMess)
+    return modMessage
+cacheData = {}
+soc = socket(AF_INET, SOCK_STREAM) 
+soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+soc.bind(("", 12345))
+soc.listen(5) 
+while True:
+    print '[Proxy Server]: Listening for client requests ...'
+    clientSoc, addr = soc.accept()
+    print '[Proxy Server]: Received connection from client at: ', addr
+    # message = recvResponse(clientSoc)
+    request = clientSoc.recv(1024)
+    print request
+    # parsing request for filename
+    filename = parseRequest(request)
+    # Make socket for connection to the final server
     
-    serverSoc = socket(AF_INET, SOCK_STREAM) #Create a socket on the proxyserver
+    serverSoc = socket(AF_INET, SOCK_STREAM) 
     host,port = filename.split("/")[1].split(":")
-    serverSoc.connect((host,int(port)))
-    try: #Check whether the file exists in the cache
-        # f = open(filetouse[1:], "r")
-        # f = open(filename, "r")
+    print "[Proxy Server]: Connecting to ",host," at port ",port
+    try:
+        serverSoc.connect((host,int(port)))
+    except:
+        print "[Proxy Server]: Error with connection to server"
+    try:
         val = cacheData[filename]
-        # outputdata = f.readlines()
         ind = val.find("Last-Modified: ")
         if ind != -1:
-            timestamp = val[ind+15:ind+15+29]
-            tempTime = time.strptime(timestamp, "%a, %d %b %Y %H:%M:%S %Z")
-            tempTime = time.localtime(time.mktime(tempTime) + 3600*5.5)
-            timestamp = time.strftime("%a %b %d %H:%M:%S %Y",tempTime)
-            IMSheader = "If-Modified-Since: " + timestamp
-            splitMess = message.split('\n')
-            splitMess.insert(3,IMSheader)
-            modMessage = '\n'.join(splitMess)
-            modMessage = modMessage.replace("http://127.0.0.1:20000","")
+            IMSheader = getIMSHeader(ind,val)   
+            modMessage = addHeader(request,IMSheader)
+            modMessage = modMessage.replace("http://" + host + ":" + port,"")
             serverSoc.send(modMessage)
-            response = ""
-            while True:
-                resp = serverSoc.recv(1024)
-                if not resp:
-                    break
-                response += resp
-                print resp
-            print "ethe"
+            response = recvResponse(serverSoc)
+            print "Server Response:\n",response
             if response.find("HTTP/1.0 304 Not Modified") != -1:
-                cData = cacheData[filename]
-                clientSoc.send(cData)
-                print "Cached data sent"
+                clientSoc.send(cacheData[filename])
+                print "[Proxy Sever]: Cached data sent!"
             else:
-                print "Caching data"
+                print "[Proxy Server]: Updating Cached data!"
                 cacheData[filename] = response
                 clientSoc.send(response)
-        #ProxyServer finds a cache hit and generates a response message
-        # clientSoc.send("HTTP/1.0 200 OK\r\n")
-        # clientSoc.send("Content-Type:text/html\r\n")
-        # for data in outputdata:
-        #     clientSoc.send(data)
-        # print 'Read from cache'
-    except KeyError: #Error handling for file not found in cache
+    except KeyError:
         try:
-            message = message.replace("http://127.0.0.1:20000","")
-            serverSoc.send(message)
-            # c.send("GET " + "/" + filename + " HTTP/1.1\r\n")
-            response = ""
-            print "ehte vi"
-            while True:
-                resp = serverSoc.recv(1024)
-                if not resp:
-                    break
-                clientSoc.send(resp)
-                response += resp
-            print response
+            request = request.replace("http://" + host + ":" + port,"")
+            serverSoc.send(request)
+            response = recvResponse(serverSoc)
+            print "Server Response:\n",response
+            clientSoc.send(response)
             if response.find("Cache-control: no-cache") > 0:
-                print "file not cached"
+                print "[Proxy Server]: Data not cached!"
             else:
+                print "[Proxy Server]: Caching data!"
                 cacheData[filename] = response
         except:
-            print "Illegal request"
-    clientSoc.close() #Close the client and the server sockets
+            print "[Proxy Server]: Error handling request"
+    clientSoc.close() 
 
 
